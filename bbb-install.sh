f3564282730ddb13ed1562b3f6b311cd8b45a0d5
Anton Georgiev <antobinary@users.noreply.github.com>
2024-05-28 11:12:20 -0400

    fix: avoid overwriting postgresql.gpg

--- bbb-install.sh
+++ bbb-install.sh
@@ -314,7 +314,9 @@ main() {
 
     # postgres for BigBlueButton core
     sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-    curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+    if [ ! -f /etc/apt/trusted.gpg.d/postgresql.gpg ]; then
+      curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+    fi
 
     touch /root/.rnd
     MONGODB=mongodb-org
