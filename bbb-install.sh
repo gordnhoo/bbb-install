fa2719a1a9095c8425ef11570f63eaf06172d817
Anton Georgiev <antobinary@users.noreply.github.com>
2024-08-09 20:05:15 -0400

    drop existing nodesource.gpg

--- bbb-install.sh
+++ bbb-install.sh
@@ -298,6 +298,9 @@ main() {
     fi
     if [ ! -f /etc/apt/sources.list.d/nodesource.list ]; then
       sudo mkdir -p /etc/apt/keyrings
+      if [ -f /etc/apt/keyrings/nodesource.gpg ]; then
+        rm /etc/apt/keyrings/nodesource.gpg
+      fi
       curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
       NODE_MAJOR=22
       echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
